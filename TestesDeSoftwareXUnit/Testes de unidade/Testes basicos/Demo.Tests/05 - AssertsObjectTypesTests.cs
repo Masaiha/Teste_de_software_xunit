6669a26c9d0ef51adfe115f5bf8034f0a11f4c05
Masaiha <masanori.iha@gmail.com>
2021-03-02 06:49:43 -0300

    Aprendendo Asserts para Objects Type

---
+++ TestesDeSoftwareXUnit/Testes de unidade/Testes basicos/Demo.Tests/05 - AssertsObjectTypesTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class _05___AssertsObjectTypesTests
+    {
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoFuncionario()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Masanori", 10000);
+
+            // Asserts
+            Assert.IsType<Funcionario>(funcionario);
+        }
+
+        /// <summary>
+        /// Testando herança
+        /// </summary>
+        [Fact]
+        public void FuncionarioFactory_Criar_DeveRetornarTipoDerivadoPessoa()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Masanori", 10000);
+
+            // Asserts
+            Assert.IsAssignableFrom<Pessoa>(funcionario);
+        }
+    }
+}
