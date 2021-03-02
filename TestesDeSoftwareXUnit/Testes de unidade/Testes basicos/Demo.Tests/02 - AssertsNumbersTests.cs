9d9753ea984d0de79d9dc65b1aa16610817bd372
Masaiha <masanori.iha@gmail.com>
2021-03-01 21:09:42 -0300

    Aprendendo Asserts para Number

---
+++ TestesDeSoftwareXUnit/Testes de unidade/Testes basicos/Demo.Tests/02 - AssertsNumbersTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class _02___AssertsNumbersTests
+    {
+        [Fact]
+        public void Calculadora_Somar_DeveSerIgual()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(1,1);
+
+            // Asserts
+            Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void Calculadora_Somar_NaoDeveSerIgual()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(1.11111111, 2.1666666);
+
+            // Asserts
+            Assert.NotEqual(3.3, resultado, 1);
+        }
+    }
+}
