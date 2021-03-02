84f375691e299b48425d0a2f79aa8051aa7cc94a
Masaiha <masanori.iha@gmail.com>
2021-03-02 07:04:19 -0300

    Aprendendo Asserts para Exceptions

---
+++ TestesDeSoftwareXUnit/Testes de unidade/Testes basicos/Demo.Tests/07 - AssertsExceptionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class _07___AssertsExceptionsTests
+    {
+        [Fact]
+        public void Calculadora_Dividir_DeveRetornarErroDivisaoPorZero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act & Asserts
+            Assert.Throws<DivideByZeroException>(() => calculadora.Dividir(10, 0));
+        }
+
+        [Fact]
+        public void Funcionario_Salario_DeveRetornarErroSalarioInferiorPermitodo()
+        {
+            // Arrange & Act & Asserts
+            var exception =
+                Assert.Throws<Exception>(() => FuncionarioFactory.Criar("Masanori", 250));
+
+            Assert.Equal("Salario inferior ao permitido", exception.Message);
+        }
+
+    }
+}
