3cd1d1bf972b649cef495cbef88b7018219c6a27
Masaiha <masanori.iha@gmail.com>
2021-03-02 07:45:11 -0300

    Aprendendo Fixture
    Método de criar uma classe que instância objetos que serão reutilizados em vários testes

---
+++ TestesDeSoftwareXUnit/Testes de unidade/Features/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    /// <summary>
+    /// Classe criada como uma coleção que será injetado nas classes que forem
+    /// reutilizar o ClienteTestsFixture
+    /// </summary>
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture> {}
+
+    /// <summary>
+    /// Como é uma classe reutilizável, deve, então implementar o Disposable
+    /// </summary>
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Eduardo",
+                "Pires",
+                DateTime.Now.AddYears(-30),
+                "edu@edu.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "edu2edu.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose() { }
+    }
+}
